f51a941c863e8fb03c934acc08ab634aebb54868
keshew <artyom.korotckov2017@yandex.ru>
2025-11-13 13:03:31 +0500

    Initial Commit

---
+++ CasNewGen/CasNewGenApp.swift
@@ -0,0 +1,17 @@
+//
+//  CasNewGenApp.swift
+//  CasNewGen
+//
+//  Created by Артём Коротков on 13.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct CasNewGenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
